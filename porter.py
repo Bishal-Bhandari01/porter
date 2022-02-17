20032564077954f22b81fadadad9b8c426cc3023
James-smith2 <80243759+James-smith2@users.noreply.github.com>
2022-02-17 07:51:58 +0545

    add udp scan

--- porter.py
+++ porter.py
@@ -9,22 +9,38 @@ else:
     print("Invalid amount of arguments.")
     print("python3 porter.py <ip>")
 
-print("*"*50)
-print("Scanning started")
-print("*"*50)
-
 try:
     n = int(input("Select Ports to scan (0-65535): "))
+    pro = input("Select protocol (TCP/UDP: )")
 
-    for port in range(n):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result = s.connect_ex((target, port))
+    print("*"*50)
+    print("Scanning started")
+    print("*"*50)
 
-        if result==0:
-            print("Port {} is open".format(port))
+    if pro == "tcp" or pro == "TCP":
+        for port in range(n+1):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((target, port))
+
+            if result==0:
+                print("Port {} is open".format(port))
+        
+            s.close()
+            pass
     
-        s.close()
+    if pro=="udp" or pro=="UDP":
+        for port in range(n+1):
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((target, port))
+
+            if result==0:
+                print("Port {} is open".format(port))
+    
+            s.close()
+            pass
+
 
 except KeyboardInterrupt:
     print("\nExisting Program...")
